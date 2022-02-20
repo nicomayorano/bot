dbb3c3ecbfda5757dd4b48f6433f5f36eaa1b7a0
nicomayorano <nicolasmayorano@gmail.com>
2022-02-20 18:33:25 -0300

    prettier

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,22 +1,36 @@
 // Imports
-import { cuandoJuega } from '../api-football/cuandojuega.js';
+import { cuandoJuega } from "../api-football/cuandojuega.js";
 
 // Constants
-const MESSAGE_COMMAND_SUFFIX = '!';
+const MESSAGE_COMMAND_SUFFIX = "!";
 
 export default {
-	name: 'messageCreate',
-	async execute(message) {
-        if (!message.content.startsWith(MESSAGE_COMMAND_SUFFIX)) return;
-		let args = message.content.substring(1).replace(/\s+/g, ' ').trim().split(' '); //removes suffix and repeated whitespaces, splits in an array of strings
+  name: "messageCreate",
+  async execute(message) {
+    if (!message.content.startsWith(MESSAGE_COMMAND_SUFFIX)) return;
+    let args = message.content
+      .substring(1)
+      .replace(/\s+/g, " ")
+      .trim()
+      .split(" "); //removes suffix and repeated whitespaces, splits in an array of strings
 
-        switch(args[0]) {
-			case 'cuandojuega':
-				let reply = await cuandoJuega(args);
-				message.reply(reply)
-				.then(() => console.log(`Replied to message "${message.content}"`))
-  				.catch(console.error);
-				break;	
-		}
-	}
-}
+    switch (args[0]) {
+      case "cuandojuega":
+        let reply = await cuandoJuega(args);
+        message
+          .reply(reply)
+          .then(() => console.log(`Replied to message "${message.content}"`))
+          .catch(console.error);
+        break;
+
+      case "precio":
+        let reply = await cuandoJuega(args);
+        message
+          .reply(reply)
+          .then(() => console.log(`Replied to message "${message.content}"`))
+          .catch(console.error);
+        // Powered by CoinGecko
+        break;
+    }
+  },
+};
--- index.js
+++ index.js
@@ -3,21 +3,24 @@
 //=======================//
 
 // Imports and instances
-import { Client, Intents } from 'discord.js';
-import config from './config.js';
-import fs from 'fs';
+import { Client, Intents } from "discord.js";
+import config from "./config.js";
+import fs from "fs";
 
-const discordClient = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const discordClient = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 discordClient.login(config.token);
 
-
 //Event handling
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 for (const file of eventFiles) {
-	const { default: event } = await import(`./events/${file}`);
-	if (event.once) {
-		discordClient.once(event.name, (...args) => event.execute(...args));
-	} else {
-		discordClient.on(event.name, (...args) => event.execute(...args));
-	}
+  const { default: event } = await import(`./events/${file}`);
+  if (event.once) {
+    discordClient.once(event.name, (...args) => event.execute(...args));
+  } else {
+    discordClient.on(event.name, (...args) => event.execute(...args));
+  }
 }
